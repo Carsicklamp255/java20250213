89ed29654d3254fc350b1783d152d258705f5542
Carsicklamp255 <160789738+Carsicklamp255@users.noreply.github.com>
2025-02-13 10:40:47 -0300

    Add files via upload

---
+++ aula20250213/src/main/java/com/mycompany/aula20250213/Aula20250213.java
@@ -0,0 +1,29 @@
+/*public class Aula20250213 {
+
+    public static void main(String[] args) {
+        System.out.println("Menu");
+        System.out.input("Soma!");
+        System.out.println("Subtração");
+        System.out.println("Multiplicação");
+        System.out.println("Divisão");
+        System.out.println("Sair");
+    }
+}
+ pessoal*/
+
+/*#copia*/
+public class Aula20250213 {
+
+    public static void main(String[] args) {
+        System.out.println("\t==================");
+        System.out.println("\t\tMENU");
+        System.out.println("\t==================");
+        System.out.println("\t[1]tSoma!");
+        System.out.println("\t[2]tSubtração");
+        System.out.println("\t[3]tMultiplicação");
+        System.out.println("\t[4]tDivisão");
+        System.out.println("\t[0]tSair");
+        
+        System.out.printf("\n\tDigite a opção desejada");
+    }
+}
